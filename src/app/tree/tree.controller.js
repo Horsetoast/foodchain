531aa41ef8306ff53fc02a4dc9fdef3e7d224461
Horsetoast <peciar.matus@gmail.com>
2016-08-19 22:35:39 +0800

    Main screen add items layout

---
+++ src/app/tree/tree.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('foodchain')
+    .controller('TreeController', TreeController);
+
+  /** @ngInject */
+  function TreeController() {
+    var vm = this;
+
+  }
+})();
